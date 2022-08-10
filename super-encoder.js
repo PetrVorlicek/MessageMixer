0faec0f44e74a44951154a36f00db5aeb572253f
PetrVorlicek <vorlicek.petricek@gmail.com>
2022-08-10 15:58:50 +0200

    Add super-encoder.js, with encode - decode funciton

--- super-encoder.js
+++ super-encoder.js
@@ -1,15 +1,23 @@
-// Import the encryptors functions here.
 
+const {caesarCipher, symbolCipher, reverseCipher} = require('./encryptors');
 
 const encodeMessage = (str) => {
     // Use the encryptor functions here.
-    
-  }
+    str = symbolCipher(str);
+    str = reverseCipher(str);
+    str = caesarCipher(str, 12);
+    console.log(`Your encoded message is: ${str}`);
+    return str;
+  };
   
   const decodeMessage = (str) => {
     // Use the encryptor functions here.
-    
-  }
+    str = caesarCipher(str, -12);  
+    str = reverseCipher(str);
+    str = symbolCipher(str);
+    console.log(`Your decoded message is: ${str}`);
+    return str;
+  };
   
   // User input / output.
   
